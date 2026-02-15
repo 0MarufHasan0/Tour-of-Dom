ee4425c56ca3aabc14367944913eca0810eadbc1
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-02-15 23:38:57 +0600

    cssin.js

---
+++ js/cssin.js
@@ -0,0 +1,27 @@
+// console.log('file connected')
+
+// Eyta dia all section and tar class name id  name pabo
+// HTML collection list
+
+// const sections  =document.getElementsByTagName('section');
+
+ 
+// Node  List
+const sections = document.querySelectorAll('section')
+// console.log(sections);
+// for(const section of sections){
+//     console.log(section);
+//     section.style.backgroundColor='lightblue'
+//     section.style.border='3px  solid yellow'
+//     section.style.marginBottom='5px'
+//     section.style.borderRadius='10px'
+//     section.style.padding='20px'
+// }
+
+
+// ClassList add css theke kono ekta class likhe eykhane add kora
+// dymanic class
+
+for (const section of sections){
+    section.classList.add('section-card')
+}
